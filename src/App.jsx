6572f1ca3842725765466c230c690ae935183afa
Giannis Ersov <giannis.ersov@gmail.com>
2026-03-08 17:42:54 +0200

    feat: routing has been finilised

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 
+
+import Layout from './components/Layout.jsx';
+
 import WeekView from './pages/WeekView.jsx';
 import AddView from './pages/AddView.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
@@ -7,16 +10,15 @@ import PageNotFound from './pages/PageNotFound.jsx';
 function App() {
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <WeekView />
+    path: "/",
+    element: <Layout />,
+    children: [
+      {index: true, element: <WeekView />},
+      {path: "week", element: <WeekView />},
+      {path: "add", element: <AddView />},
+    ]
     },
-    {
-      path: "/Add",
-      element: <AddView />
-    },{
-      path: "*",
-      element: <PageNotFound />
-    }
+    {path: "*", element: <PageNotFound />}
   ]);
 
   return (
